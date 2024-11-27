788b1b97d1150a52613db1bc4d09f27c43551ed0
DerBlockProgrammer <marcvossler@outlook.de>
2024-11-27 11:08:36 +0100

    first commit

---
+++ src/app/main-content/landing-page/landingPage.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { NavbarComponent} from './navbar/navbar.component';
+
+
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent ],
+    template: `
+    <section>
+    <div> <app-navbar></app-navbar></div>
+    <h1 class="fontRaleway">Sakura Ramen</h1>
+    <h2 class="fontRaleway">Best Ramen in Town</h2>
+    </section>
+    `,
+    styleUrl: './landingPage.component.scss'
+})
+export class LandingPageComponent {
+
+}
